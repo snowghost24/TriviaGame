90324f49da9344258abbc00cba6be3a59c0c4c15
Snowghost24 <Snowghost@Joses-MacBook-Air.local>
2017-08-31 22:45:18 -0400

    dried the Javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,20 @@
-// Timer Function
+// This funtion fires every second to create a countdonw
 function countDown(secs, elem) {
    var element = document.querySelector("#status");
    element.innerHTML = secs;
    secs--;
    var timer = setTimeout('countDown(' + secs + ',"' + elem + '")', 1000)
-   window.timer = timer;
+	window.timer = timer;
+	// This function pulls up the next question after time has expired
    if (secs <= -1) {
       clearTimeout(timer);
-      function onQuestion(received) {
          wrongAnswers++;
          countDown(20, "status");
          var newNumb = parseInt(eval(switcher + 1));
          console.log(newNumb);
          displayVals(allDisplays[newNumb]);
       }
-      onQuestion(page.question);
    }
-}
 
 		function displayer(question, rightAnswer, wrongAnswer1, wrongAnswer2, wrongAnswer3, gif) {
 			this.question = question;
@@ -36,245 +34,136 @@ function countDown(secs, elem) {
 		var display8 = new displayer("Which player has had the most points in an overtime period?", "Penny Hardaway", "Dirk Nowitzki", "Kobe Bryant", "Gilbert Arenas", '<img src="assets/images/arenas.gif" />');
 		var display9 = new displayer("Who is the NBA all-time leader in assist?", "Magic Johnson", "Steve Nash", " Rajon Rondo", "John Stockton", '<img src="assets/images/stockton.gif" />');
 		var display10 = new displayer("Which player had 4 consecutive 40 point games as a rookie?", "Wilt Chamberlain", "Allen Iverson", "Michael Jordan", "Charles Barkley", '<img src="assets/images/ai-mj.gif" />');
-
 		var allDisplays = [display1,display2,display3,display4,display5,display6,display7,display8,display9,display10];
 		var rightAnswer = 0;
 		var wrongAnswers = 0;
 		var switcher = 0;
 		$(document).ready(function () {
 			//Start of jQuery////////////////////////////////////////////////////////////////////////////
-
-			// Displays question on the screen and answers schreen
+			// Displays questions and answers on the screen
+			// Displays count of right and wrong answers and hides previous giphy
 			window.displayVals = function (page) {
+				window.page = page;
             switcher++;
-            console.log(page);
 				$("#answers").show();
-				$(".right").html("Right Answers: " + rightAnswer).fadeIn()
-				$(".wrong").html("Wrong Answers: " + wrongAnswers).fadeIn()
+				$(".right").html("Right Answers: " + rightAnswer);
+				$(".wrong").html("Wrong Answers: " + wrongAnswers);
 				$(".gif-spot").hide();
 				$("h4").remove();
+				
 				$.each(page, function (key, value) {
 					if (key == "question") {
 						$("#question").html("<h3>" + value + "</h3>");
-						// console.log(display1["righAnswer"]);
 					} else if (key !== "gif") {
                   $("#answers").append("<h4>" + value + "</h4>");
 					} 
 				});
-				window.page = page;
 			}
 
-
-			// This call starts the trivia game
+			// Starts the trivia game
 			$("#starter-btn").on("click", function () {
 				countDown(20, "status");
 				displayVals(display1);
 				$("#starter-btn").hide();
 			})
 
-         // restarts the game
+         // restarts the game and resets variables
 			$("#restart-btn").on("click", function () {
-				$("#restart-btn").hide();
 				rightAnswer = 0;
 				wrongAnswers = 0;
 				switcher = 0;
-				countDown(20, "status");
+				$("#restart-btn").hide();
 				$("#question").show();
-					$(".final").hide();
+				$(".final").hide();
 				displayVals(display1);
+				countDown(20, "status");
 			})
-
-
 
 			document.querySelector("#problem").addEventListener("click", (e) => {
 				$(".gif-spot").show();
 				$("#answers").hide();
 				var hitTarget = e.target.innerHTML;
-				if (switcher == 1 && hitTarget == "Steph Curry") {
+				//right answer handler
+				function qRight (firstDisplayNumb,secondDisplayNumb,gifDuration){
 					rightAnswer++
-					
-					$(".gif-spot").html(display1.gif).animate({ bottom: '250px' }).fadeIn()
+					$(".gif-spot").html(firstDisplayNumb.gif);
 					clearTimeout(timer);
 					setTimeout(function () {
-						displayVals(display2);
+						displayVals(secondDisplayNumb,);
 						countDown(18, "status");
-					}, 2600)
-				} else if (switcher == 1 && hitTarget !== "Steph Curry") {
+					}, gifDuration)
+				}
+				// wrong answer handler
+				function qWrong(correctAns,theGif,secondDisplayNumb, gifDuration) {
 					wrongAnswers++;
-					$(".correct").html("<h1>The correct answer is Stephen Curry</h1>").show();
-					$(".gif-spot").html('<img src="assets/images/jamesh.gif" />');
+					$(".correct").html("<h1>"+correctAns+"</h1>").show();
+					$(".gif-spot").html(theGif);
 					clearTimeout(timer);
 					setTimeout(function () {
 						$(".correct").hide();
-						displayVals(display2);
+						displayVals(secondDisplayNumb);
 						countDown(18, "status");
-					}, 1800)
+					}, gifDuration)
+				}
+
+				//Handles users response
+				if (switcher == 1 && hitTarget == "Steph Curry") {
+					qRight(display1,display2,2600);
+				} else if (switcher == 1 && hitTarget !== "Steph Curry") {
+					qWrong("The correct answer is Stephen Curry", '<img src="assets/images/jamesh.gif" />',display2,1800);
 				}
 
 				if (switcher == 2 && hitTarget == "Ray Allen") {
-					rightAnswer++;
-					$(".gif-spot").html(display2.gif);
-					clearTimeout(timer);
-					setTimeout(function () {
-						countDown(18, "status");
-						displayVals(display3);
-					}, 5000)
+					qRight(display2, display3, 5000);
 				} else if (switcher == 2 && hitTarget !== "Ray Allen") {
-					wrongAnswers++;
-					$(".correct").html("<h1>The correct answer is Ray Allen</h1>").show();
-					$(".gif-spot").html('<img src="assets/images/rondoshock.gif" />');
-					clearTimeout(timer);
-					setTimeout(function () {
-						$(".correct").hide();
-						displayVals(display3);
-						countDown(18, "status");
-					}, 3000)
+					qWrong("The correct answer is Ray Allen", '<img src="assets/images/rondoshock.gif" />', display3, 3000);
 				}
 
 				if (switcher == 3 && hitTarget == "Steph Curry") {
-					rightAnswer++;
-					$(".gif-spot").html(display3.gif);
-					clearTimeout(timer);
-					setTimeout(function () {
-						displayVals(display4);
-						countDown(18, "status");
-					}, 5800)
+					qRight(display3, display4, 5800);
 				} else if (switcher == 3 && hitTarget !== "Steph Curry") {
-					wrongAnswers++;
-					$(".correct").html("<h1>The correct answer is Steph Curry</h1>").show();
-					$(".gif-spot").html('<img src="assets/images/westbrookface.gif" />');
-					clearTimeout(timer);
-					setTimeout(function () {
-						$(".correct").hide();
-						displayVals(display4);
-						countDown(18, "status");
-					}, 2200)
+					qWrong("The correct answer is Stephen Curry", '<img src="assets/images/westbrookface.gif" />', display4, 2200);
 				}
 
 
 				if (switcher == 4 && hitTarget == "Klay Thompson") {
-					rightAnswer++;
-					$(".gif-spot").html(display4.gif);
-					clearTimeout(timer);
-					setTimeout(function () {
-						displayVals(display5);
-						countDown(18, "status");
-					}, 5800)
+					qRight(display4, display5, 5800);
 				} else if (switcher == 4 && hitTarget !== "Klay Thompson") {
-					wrongAnswers++;
-					$(".correct").html("<h1>The correct answer is Klay Thompson</h1>").show();
-					$(".gif-spot").html('<img src="assets/images/kalyNah.gif" />');
-					clearTimeout(timer);
-					setTimeout(function () {
-						$(".correct").hide();
-						displayVals(display5);
-						countDown(18, "status");
-					}, 3000)
+					qWrong("The correct answer is Klay Thompson", '<img src="assets/images/kalyNah.gif"  />', display5, 3000);
 				}
 
 
 				if (switcher == 5 && hitTarget == "Michael Jordan") {
-					rightAnswer++;
-					$(".gif-spot").html(display5.gif);
-					clearTimeout(timer);
-					setTimeout(function () {
-						displayVals(display6);
-						countDown(18, "status");
-					}, 4900)
+					qRight(display5, display6, 4900);
 				} else if (switcher == 5 && hitTarget !== "Michael Jordan") {
-					wrongAnswers++;
-					$(".correct").html("<h1>The correct answer is Michael Jordan</h1>").show();
-					$(".gif-spot").html('<img src="assets/images/jimmyb.gif" />');
-					clearTimeout(timer);
-					setTimeout(function () {
-						$(".correct").hide();
-						displayVals(display6);
-						countDown(18, "status");
-					}, 4000)
+					qWrong("The correct answer is Michael Jordan", '<img src="assets/images/jimmyb.gif"  />', display6, 4000);
 				}
 
 				if (switcher == 6 && hitTarget == "Golden State Warriors") {
-					rightAnswer++;
-					$(".gif-spot").html(display6.gif);
-					clearTimeout(timer);
-					setTimeout(function () {
-
-						displayVals(display7);
-						countDown(18, "status");
-					}, 3000)
+					qRight(display6, display7, 3000);
 				} else if (switcher == 6 && hitTarget !== "Golden State Warriors") {
-					wrongAnswers++;
-					$(".correct").html("<h1>The correct answer is Golden State Warriors</h1>").show();
-					$(".gif-spot").html('<img src="assets/images/BlakeShock.gif" />');
-					clearTimeout(timer);
-					setTimeout(function () {
-						$(".correct").hide();
-						displayVals(display7);
-						countDown(18, "status");
-					}, 2800)
+					qWrong("Golden State Warriors silly", '<img src="assets/images/BlakeShock.gif"  />', display7, 2800);
 				}
 
 				if (switcher == 7 && hitTarget == "Kareem Abdul Jabbar") {
-					rightAnswer++;
-					$(".gif-spot").html(display7.gif);
-					clearTimeout(timer);
-					setTimeout(function () {
-						displayVals(display8);
-						countDown(18, "status");
-					}, 5800)
+					qRight(display7, display8, 3000);
 				} else if (switcher == 7 && hitTarget !== "Kareem Abdul Jabbar") {
-					wrongAnswers++;
-					$(".correct").html("<h1>The correct answer is Kareem Abdul Jabbar</h1>").show();
-					$(".gif-spot").html('<img src="assets/images/rondoshock.gif" />');
-					clearTimeout(timer);
-					setTimeout(function () {
-						$(".correct").hide();
-						displayVals(display8);
-						countDown(18, "status");
-					}, 3000)
+					qWrong("The correct answer is Kareem Abdul Jabbar", '<img src="assets/images/rondoshock.gif" />', display8, 3000);
 				}
 
 				if (switcher == 8 && hitTarget == "Gilbert Arenas") {
-					rightAnswer++;
-					$(".gif-spot").html(display8.gif);
-					clearTimeout(timer);
-					setTimeout(function () {
-						displayVals(display9);
-						countDown(18, "status");
-					}, 4000)
+					qRight(display8, display9, 4000);
 				} else if (switcher == 8 && hitTarget !== "Gilbert Arenas") {
-					wrongAnswers++;
-					$(".correct").html("<h1>The correct answer is Gilbert Arenas</h1>").show();
-					$(".gif-spot").html('<img src="assets/images/westbrookface.gif" />');
-					clearTimeout(timer);
-					setTimeout(function () {
-						$(".correct").hide();
-						displayVals(display9);
-						countDown(18, "status");
-					}, 2200)
+					qWrong("The correct answer is Gilbert Arenas", '<img src="assets/images/westbrookface.gif" />', display9, 2200);
 				}
 
 				if (switcher == 9 && hitTarget == "John Stockton") {
+					qRight(display9, display10, 5000);
 					rightAnswer++;
-					$(".gif-spot").html(display9.gif);
-					clearTimeout(timer);
-					setTimeout(function () {
-						displayVals(display10);
-						countDown(18, "status");
-					}, 5000)
 				} else if (switcher == 9 && hitTarget !== "John Stockton") {
-					wrongAnswers++;
-					$(".correct").html("<h1>The correct answer is John Stockton</h1>").show();
-					$(".gif-spot").html('<img src="assets/images/kobe.gif" />');
-					clearTimeout(timer);
-					setTimeout(function () {
-						$(".correct").hide();
-						displayVals(display10);
-						countDown(18, "status");
-					}, 2800)
+					qWrong("The correct answer is John Stockton", '<img src="assets/images/kobe.gif" />', display10, 2800);
 				}
-
-								
+				
 				if (switcher == 10 && hitTarget == "Allen Iverson") {
 					rightAnswer++;
 					$(".gif-spot").html(display10.gif);
@@ -284,7 +173,6 @@ function countDown(secs, elem) {
 						$("#question").hide();
 						$(".correct").hide();
 						$(".gif-spot").hide();
-						
 					}, 4300)
 				} else if (switcher == 10 && hitTarget !== "Allen Iverson") {
 					wrongAnswers++;
@@ -295,17 +183,16 @@ function countDown(secs, elem) {
 					setTimeout(function () {
 						displayScore();
 						$("#question").hide();
-							$(".correct").hide();
-							$(".gif-spot").hide();
+						$(".correct").hide();
+						$(".gif-spot").hide();
+						
 					}, 3000)
-
 
 				}
 				function displayScore() {
 						$("#answers").hide();
 						$("#question").hide();
 						$(".final").show();
-
 
 						if (rightAnswer > 7) {
 							$(".final").html("Congratulations you had " + rightAnswer + " correct answers. Ball must really be life");
